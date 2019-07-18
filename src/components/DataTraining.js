cd12f739c7601ab713c9bf609677d1b253bb540b
setiawan <setiawansocial@gmail.com>
2019-07-18 16:45:21 +0700

    fixupdate

--- src/components/DataTraining.js
+++ src/components/DataTraining.js
@@ -1,4 +1,4 @@
-// import React, { Component } from "react";
+import React, { Component } from "react";
 import axios from "axios";
 import { Route, Link, Redirect } from "react-router-dom";
 import { DataTable } from "primereact/datatable";
@@ -23,6 +23,7 @@ export class DataTraining extends Component {
         <div className="p-col-12">
           <div className="card card-w-title">
             <h1>Data Training</h1>
+            
             <table style={{ border: "1px solid black" }}>
               <thead>
                 <tr style={{ border: "1px solid black" }}>
@@ -126,6 +127,246 @@ export class DataTraining extends Component {
                 </tr>
               </tbody>
             </table>
+             
+            <p>
+              <b>Data Training</b>
+            </p>
+            <table style={{ border: "1px solid black" }}>
+              <thead>
+                <tr style={{ border: "1px solid black" }}>
+                  <th style={{ border: "1px solid black" }}>Nama</th>
+                  <th style={{ border: "1px solid black" }}>Jenis Kelamin</th>
+                  <th style={{ border: "1px solid black" }}>Alamat</th>
+                  <th style={{ border: "1px solid black" }}>Tmp. Lahir</th>
+                  <th style={{ border: "1px solid black" }}>Tgl. Lahir</th>
+                  <th style={{ border: "1px solid black" }}>Umur</th>
+                  <th style={{ border: "1px solid black" }}>Pekerjaan</th>
+                  <th style={{ border: "1px solid black" }}>Penghasilan</th>
+                  <th style={{ border: "1px solid black" }}>Status Rumah</th>
+                  <th style={{ border: "1px solid black" }}>Status Marital</th>
+                  <th style={{ border: "1px solid black" }}>Jumlah Anak</th>
+                  <th style={{ border: "1px solid black" }}>
+                    Metode Pembayaran
+                  </th>
+                  <th style={{ border: "1px solid black" }}>Perpanjang</th>
+                  <th style={{ border: "1px solid black" }}>Kategori</th>
+                </tr>
+              </thead>
+              <tbody style={{ border: "1px solid black" }}>
+                <tr style={{ border: "1px solid black" }}>
+                  <td style={{ border: "1px solid black" }}>Robby Risjad</td>
+                  <td style={{ border: "1px solid black" }}>Pria</td>
+                  <td style={{ border: "1px solid black" }}>Jakarta Pusat</td>
+                  <td style={{ border: "1px solid black" }}>Jakarta</td>
+                  <td style={{ border: "1px solid black" }}>04-Jun-95</td>
+                  <td style={{ border: "1px solid black" }}>19 -24</td>
+                  <td style={{ border: "1px solid black" }}>PNS</td>
+                  <td style={{ border: "1px solid black" }}> > 15 Juta</td>
+                  <td style={{ border: "1px solid black" }}>Sendiri Kredit</td>
+                  <td style={{ border: "1px solid black" }}>Belum Menikah</td>
+                  <td style={{ border: "1px solid black" }}>0</td>
+                  <td style={{ border: "1px solid black" }}>Credit Card</td>
+                  <td style={{ border: "1px solid black" }}>Ya</td>
+                  <td style={{ border: "1px solid black" }}>Member</td>
+                </tr>
+                <tr style={{ border: "1px solid black" }}>
+                  <td style={{ border: "1px solid black" }}>
+                    Dolly Periagutan
+                  </td>
+                  <td style={{ border: "1px solid black" }}>Pria</td>
+                  <td style={{ border: "1px solid black" }}>Jakarta Selatan</td>
+                  <td style={{ border: "1px solid black" }}>Bandung</td>
+                  <td style={{ border: "1px solid black" }}>17-Jan-95</td>
+                  <td style={{ border: "1px solid black" }}>19 -24</td>
+                  <td style={{ border: "1px solid black" }}>Wiraswasta</td>
+                  <td style={{ border: "1px solid black" }}> > 15 Juta</td>
+                  <td style={{ border: "1px solid black" }}>Orang Tua</td>
+                  <td style={{ border: "1px solid black" }}>Belum Menikah</td>
+                  <td style={{ border: "1px solid black" }}>0</td>
+                  <td style={{ border: "1px solid black" }}>Debit Card</td>
+                  <td style={{ border: "1px solid black" }}>Tidak</td>
+                  <td style={{ border: "1px solid black" }}>Member</td>
+                </tr>
+                <tr style={{ border: "1px solid black" }}>
+                  <td style={{ border: "1px solid black" }}>Hari Rudy</td>
+                  <td style={{ border: "1px solid black" }}>Pria</td>
+                  <td style={{ border: "1px solid black" }}>Jakarta Pusat</td>
+                  <td style={{ border: "1px solid black" }}>Jakarta</td>
+                  <td style={{ border: "1px solid black" }}>26-May-89</td>
+                  <td style={{ border: "1px solid black" }}>25 -30</td>
+                  <td style={{ border: "1px solid black" }}>Wiraswasta</td>
+                  <td style={{ border: "1px solid black" }}> > 15 Juta</td>
+                  <td style={{ border: "1px solid black" }}>Sendiri Lunas</td>
+                  <td style={{ border: "1px solid black" }}>Menikah</td>
+                  <td style={{ border: "1px solid black" }}>2</td>
+                  <td style={{ border: "1px solid black" }}>Credit Card</td>
+                  <td style={{ border: "1px solid black" }}>Tidak</td>
+                  <td style={{ border: "1px solid black" }}>Member</td>
+                </tr>
+                <tr style={{ border: "1px solid black" }}>
+                  <td style={{ border: "1px solid black" }}>Susilo Putri</td>
+                  <td style={{ border: "1px solid black" }}>Wanita</td>
+                  <td style={{ border: "1px solid black" }}>
+                    Tangerang Selatan
+                  </td>
+                  <td style={{ border: "1px solid black" }}>Tangerang</td>
+                  <td style={{ border: "1px solid black" }}>04-Dec-93</td>
+                  <td style={{ border: "1px solid black" }}>25 -30</td>
+                  <td style={{ border: "1px solid black" }}>Karyawan Swasta</td>
+                  <td style={{ border: "1px solid black" }}> 5 - 10 Juta</td>
+                  <td style={{ border: "1px solid black" }}>Sendiri Kredit</td>
+                  <td style={{ border: "1px solid black" }}>Belum Menikah</td>
+                  <td style={{ border: "1px solid black" }}>0</td>
+                  <td style={{ border: "1px solid black" }}>Debit Card</td>
+                  <td style={{ border: "1px solid black" }}>Tidak</td>
+                  <td style={{ border: "1px solid black" }}>Non-Member</td>
+                </tr>
+                <tr style={{ border: "1px solid black" }}>
+                  <td style={{ border: "1px solid black" }}>Diono Nurjadi</td>
+                  <td style={{ border: "1px solid black" }}>Pria</td>
+                  <td style={{ border: "1px solid black" }}>Jakarta Barat</td>
+                  <td style={{ border: "1px solid black" }}>Malang</td>
+                  <td style={{ border: "1px solid black" }}>29-Nov-95</td>
+                  <td style={{ border: "1px solid black" }}>19 -24</td>
+                  <td style={{ border: "1px solid black" }}>Karyawan Swasta</td>
+                  <td style={{ border: "1px solid black" }}> 5 - 10 Juta</td>
+                  <td style={{ border: "1px solid black" }}>Orang Tua</td>
+                  <td style={{ border: "1px solid black" }}>Belum Menikah</td>
+                  <td style={{ border: "1px solid black" }}>0</td>
+                  <td style={{ border: "1px solid black" }}>Debit Card</td>
+                  <td style={{ border: "1px solid black" }}>Ya</td>
+                  <td style={{ border: "1px solid black" }}>Non-Member</td>
+                </tr>
+                <tr style={{ border: "1px solid black" }}>
+                  <td style={{ border: "1px solid black" }}>Indra Chung</td>
+                  <td style={{ border: "1px solid black" }}>Pria</td>
+                  <td style={{ border: "1px solid black" }}>Jakarta Pusat</td>
+                  <td style={{ border: "1px solid black" }}>Bandung</td>
+                  <td style={{ border: "1px solid black" }}>27-Mar-91</td>
+                  <td style={{ border: "1px solid black" }}>25 -30</td>
+                  <td style={{ border: "1px solid black" }}>Karyawan Swasta</td>
+                  <td style={{ border: "1px solid black" }}> 11 - 15 Juta</td>
+                  <td style={{ border: "1px solid black" }}>Sendiri Kredit</td>
+                  <td style={{ border: "1px solid black" }}>Menikah</td>
+                  <td style={{ border: "1px solid black" }}>1</td>
+                  <td style={{ border: "1px solid black" }}>Debit Card</td>
+                  <td style={{ border: "1px solid black" }}>Tidak</td>
+                  <td style={{ border: "1px solid black" }}>Member</td>
+                </tr>
+                <tr style={{ border: "1px solid black" }}>
+                  <td style={{ border: "1px solid black" }}>Dina Eldelina</td>
+                  <td style={{ border: "1px solid black" }}>Wanita</td>
+                  <td style={{ border: "1px solid black" }}>
+                    Tangerang Selatan
+                  </td>
+                  <td style={{ border: "1px solid black" }}>Jakarta</td>
+                  <td style={{ border: "1px solid black" }}>03-Feb-94</td>
+                  <td style={{ border: "1px solid black" }}>25 -30</td>
+                  <td style={{ border: "1px solid black" }}>Karyawan Swasta</td>
+                  <td style={{ border: "1px solid black" }}> 11 - 15 Juta</td>
+                  <td style={{ border: "1px solid black" }}>Sendiri Kredit</td>
+                  <td style={{ border: "1px solid black" }}>Menikah</td>
+                  <td style={{ border: "1px solid black" }}>1</td>
+                  <td style={{ border: "1px solid black" }}>Debit Card</td>
+                  <td style={{ border: "1px solid black" }}>Ya</td>
+                  <td style={{ border: "1px solid black" }}>Member</td>
+                </tr>
+                <tr style={{ border: "1px solid black" }}>
+                  <td style={{ border: "1px solid black" }}>Jackson</td>
+                  <td style={{ border: "1px solid black" }}>Pria</td>
+                  <td style={{ border: "1px solid black" }}>Jakarta Pusat</td>
+                  <td style={{ border: "1px solid black" }}>Surabaya</td>
+                  <td style={{ border: "1px solid black" }}>28-Dec-89</td>
+                  <td style={{ border: "1px solid black" }}>25 -30</td>
+                  <td style={{ border: "1px solid black" }}>PNS</td>
+                  <td style={{ border: "1px solid black" }}> > 15 Juta</td>
+                  <td style={{ border: "1px solid black" }}>Sendiri Lunas</td>
+                  <td style={{ border: "1px solid black" }}>Menikah</td>
+                  <td style={{ border: "1px solid black" }}>2</td>
+                  <td style={{ border: "1px solid black" }}>Credit Card</td>
+                  <td style={{ border: "1px solid black" }}>Tidak</td>
+                  <td style={{ border: "1px solid black" }}>Member</td>
+                </tr>
+                <tr style={{ border: "1px solid black" }}>
+                  <td style={{ border: "1px solid black" }}>Ivan Setiawan</td>
+                  <td style={{ border: "1px solid black" }}>Pria</td>
+                  <td style={{ border: "1px solid black" }}>Jakarta Selatan</td>
+                  <td style={{ border: "1px solid black" }}>Jakarta</td>
+                  <td style={{ border: "1px solid black" }}>29-Jul-94</td>
+                  <td style={{ border: "1px solid black" }}>25 -30</td>
+                  <td style={{ border: "1px solid black" }}>Karyawan Swasta</td>
+                  <td style={{ border: "1px solid black" }}> 5 - 10 Juta</td>
+                  <td style={{ border: "1px solid black" }}>Sendiri Kredit</td>
+                  <td style={{ border: "1px solid black" }}>Belum Menikah</td>
+                  <td style={{ border: "1px solid black" }}>2</td>
+                  <td style={{ border: "1px solid black" }}>Debit Card</td>
+                  <td style={{ border: "1px solid black" }}>Tidak</td>
+                  <td style={{ border: "1px solid black" }}>Non-Member</td>
+                </tr>
+                <tr style={{ border: "1px solid black" }}>
+                  <td style={{ border: "1px solid black" }}>Doni Irawan</td>
+                  <td style={{ border: "1px solid black" }}>Pria</td>
+                  <td style={{ border: "1px solid black" }}>
+                    Tanggerang Selatan
+                  </td>
+                  <td style={{ border: "1px solid black" }}>Bandung</td>
+                  <td style={{ border: "1px solid black" }}>16-Feb-91</td>
+                  <td style={{ border: "1px solid black" }}>25 -30</td>
+                  <td style={{ border: "1px solid black" }}>PNS</td>
+                  <td style={{ border: "1px solid black" }}> > 15 Juta</td>
+                  <td style={{ border: "1px solid black" }}>Sendiri Kredit</td>
+                  <td style={{ border: "1px solid black" }}>Belum Menikah</td>
+                  <td style={{ border: "1px solid black" }}>2</td>
+                  <td style={{ border: "1px solid black" }}>Credit Card</td>
+                  <td style={{ border: "1px solid black" }}>Ya</td>
+                  <td style={{ border: "1px solid black" }}>Member</td>
+                </tr>
+              </tbody>
+            </table>
+            <p>
+              <b>Data Test</b>
+            </p>
+            <table style={{ border: "1px solid black" }}>
+              <thead>
+                <tr style={{ border: "1px solid black" }}>
+                  <th style={{ border: "1px solid black" }}>Nama</th>
+                  <th style={{ border: "1px solid black" }}>Jenis Kelamin</th>
+                  <th style={{ border: "1px solid black" }}>Alamat</th>
+                  <th style={{ border: "1px solid black" }}>Tmp. Lahir</th>
+                  <th style={{ border: "1px solid black" }}>Tgl. Lahir</th>
+                  <th style={{ border: "1px solid black" }}>Umur</th>
+                  <th style={{ border: "1px solid black" }}>Pekerjaan</th>
+                  <th style={{ border: "1px solid black" }}>Penghasilan</th>
+                  <th style={{ border: "1px solid black" }}>Status Rumah</th>
+                  <th style={{ border: "1px solid black" }}>Status Marital</th>
+                  <th style={{ border: "1px solid black" }}>Jumlah Anak</th>
+                  <th style={{ border: "1px solid black" }}>
+                    Metode Pembayaran
+                  </th>
+                  {/* <th style={{ border: "1px solid black" }}>Perpanjang</th>
+                  <th style={{ border: "1px solid black" }}>Kategori</th> */}
+                </tr>
+              </thead>
+              <tbody style={{ border: "1px solid black" }}>
+                <tr style={{ border: "1px solid black" }}>
+                  <td style={{ border: "1px solid black" }}>Sardi Lihardo</td>
+                  <td style={{ border: "1px solid black" }}>Pria</td>
+                  <td style={{ border: "1px solid black" }}>Jakarta Barat</td>
+                  <td style={{ border: "1px solid black" }}>Jakarta</td>
+                  <td style={{ border: "1px solid black" }}>04-Jun-95</td>
+                  <td style={{ border: "1px solid black" }}>19 -24</td>
+                  <td style={{ border: "1px solid black" }}>Swasta</td>
+                  <td style={{ border: "1px solid black" }}> 5 - 10 Juta</td>
+                  <td style={{ border: "1px solid black" }}>Orang Tua</td>
+                  <td style={{ border: "1px solid black" }}>Belum Menikah</td>
+                  <td style={{ border: "1px solid black" }}>0</td>
+                  <td style={{ border: "1px solid black" }}>Credit Card</td>
+                  {/* <td style={{ border: "1px solid black" }}>Ya</td>
+                  <td style={{ border: "1px solid black" }}>Member</td> */}
+                </tr>
+              </tbody>
+            </table>
+
             <div>
               <p>
                 <b>Perhitungan Metode Naïve Bayes Pada Data Test Diatas</b>
